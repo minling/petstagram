5735be786bd52021469576e973b7a14712753535
minling <mzhao725@gmail.com>
2015-11-06 17:05:54 -0500

    adds relationships to user

--- app/User.php
+++ app/User.php
@@ -37,3 +37,13 @@ class User extends Model implements AuthenticatableContract,
      */
     protected $hidden = ['password', 'remember_token'];
 }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
+
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
