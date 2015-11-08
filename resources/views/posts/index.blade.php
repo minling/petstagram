e4a71cedb6fdf512465e697a0a3d2022e8422100
minling <mzhao725@gmail.com>
2015-11-08 10:08:12 -0500

    adds nav bar

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,10 +5,24 @@
   <h1>Posts#Index</h1>
 
   @foreach ($posts as $post)
-  <ul>    
-    <li> Title: <a href="posts/{{ $post->id }}"> {{ $post->title }} </a></li>
-    <li> Author: {{ $post->author }}</li>
-    <li>Content: {{ $post->content }}</li>
-  </ul>
+  <div class="row">
+        <div class="col s12 m7">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://www.cgdev.org/sites/default/files/cat8.jpg">
+              <span class="card-title">{{ $post->title }}</span>
+            </div>
+            <div class="card-content">
+            <li> Title: <a href="posts/{{ $post->id }}"> {{ $post->title }} </a></li>
+              <li> Title: {{ $post->title }} </li>
+              <li> Author: {{ $post->author }}</li>
+              <li>Content: {{ $post->content }}</li>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+            </div>
+          </div>
+        </div>
+      </div>
   @endforeach
 @stop
